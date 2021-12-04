16b86d4cdab9eb5de218e43ac93d41695fff757a
ichou bahba <ichou.bahba@gmail.com>
2021-12-04 15:52:46 +0100

    components part 1

---
+++ src/app/shared/dish.ts
@@ -0,0 +1,10 @@
+export class dish{
+    id: string
+    name: string
+    image: string
+    category: string
+    featured: boolean
+    label: string
+    price: string
+    description: string
+}
